8607e927ea02b654bb8d76fc41afa8e56f7d2cd2
Anthony Ettinger <anthony@chovy.com>
2021-07-24 07:09:02 -0700

    ranking

--- bin/ranking.js
+++ bin/ranking.js
@@ -22,72 +22,73 @@ mongoose.connection.on('connected', () => debug('successfully connected to db'))
 mongoose.connection.on('error', console.error);
 
 function getRankedItems(callback) {
-  Item.aggregate([
-    {
-      $lookup: {
-        from: 'users',
-        localField: 'author',
-        foreignField: '_id',
-        as: 'a',
-      },
-    },
-    {
-      $unwind: '$a',
-    },
-    {
-      $project:
+  Item.aggregate(
+    [
       {
-        _id: '$_id',
-        title: '$title',
-        url: '$url',
-        thumb: '$thumb',
-        author: '$author',
-        category: '$category',
-        score: '$score',
-        votes: '$votes',
-        comments: '$comments',
-        created: '$created',
-        views: '$views',
-        type: '$type',
-        text: '$text',
-        sponsored: '$sponsored',
-        hashtags: '$hashtags',
-        ranking: {
-          $divide: [
-            {
-              $add: [
-                '$score',
-                { $multiply: ['$comments'.length, 0.08] },
-                { $multiply: ['$views', 0.002] },
-                { $multiply: ['$a.karma', 0.0001] },
-                0.75,
-              ],
-            },
-            {
-              $add: [
-                1,
-                {
-                  $multiply: [
-                    { $divide: [{ $subtract: [new Date(), '$created'] }, 14400000] },
-                    0.4,
-                  ],
-                },
-              ],
-            },
-          ],
+        $lookup: {
+          from: 'users',
+          localField: 'author',
+          foreignField: '_id',
+          as: 'a',
         },
       },
+      {
+        $unwind: '$a',
+      },
+      {
+        $project: {
+          _id: '$_id',
+          title: '$title',
+          url: '$url',
+          thumb: '$thumb',
+          author: '$author',
+          category: '$category',
+          score: '$score',
+          votes: '$votes',
+          comments: '$comments',
+          created: '$created',
+          views: '$views',
+          type: '$type',
+          text: '$text',
+          sponsored: '$sponsored',
+          hashtags: '$hashtags',
+          ranking: {
+            $divide: [
+              {
+                $add: [
+                  '$score',
+                  { $multiply: ['$comments'.length, 0.08] },
+                  { $multiply: ['$views', 0.002] },
+                  { $multiply: ['$a.karma', 0.0001] },
+                  0.75,
+                ],
+              },
+              {
+                $add: [
+                  1,
+                  {
+                    $multiply: [
+                      { $divide: [{ $subtract: [new Date(), '$created'] }, 14400000] },
+                      0.4,
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      },
+      { $sort: { ranking: -1 } },
+      { $out: 'posts' },
+    ],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        return callback(err);
+      }
+      console.log('ranked', results);
+      process.exit(0);
     },
-    { $sort: { ranking: -1 } },
-    { $out: 'posts' },
-  ],
-  (err, results) => {
-    if (err) {
-      console.log(err);
-      return callback(err);
-    }
-    console.log('ranked', results);
-    process.exit(0);
-  });
+  ).allowDiskUse(true);
 }
 getRankedItems();
